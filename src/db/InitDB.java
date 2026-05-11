25969956c087871ae543e32d3cd21f973a7a0903
JosefMarpaung <josefchristian31@gmail.com>
2026-05-11 09:02:40 +0700

    Proyek

---
+++ src/db/InitDB.java
@@ -0,0 +1,35 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class InitDB {
+
+    public static void createTable() {
+
+        try {
+
+            Connection conn = DBConnection.connect();
+            Statement st = conn.createStatement();
+
+            String sql =
+                "CREATE TABLE IF NOT EXISTS penjualan (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "nama_pelanggan TEXT," +
+                "nomor_antrian INTEGER," +
+                "nama_menu TEXT," +
+                "harga INTEGER," +
+                "jumlah INTEGER," +
+                "total INTEGER" +
+                ")";
+
+            st.execute(sql);
+
+            System.out.println("✔ Tabel penjualan siap digunakan");
+
+        } catch (Exception e) {
+
+            System.out.println("❌ Error init DB: " + e.getMessage());
+        }
+    }
+}
